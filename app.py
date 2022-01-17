012e0ba430b9d3936b2011bd3dfa64dd1206692c
priyanka-khairnar <priyanka.khairnar@hitachivantara.com>
2022-01-17 13:57:01 +0530

    Code refactor

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """Rest API for URL Shortener."""
 
+from urllib import response
 from flask import Flask,  jsonify, request, render_template, redirect
 from util.logger_utility import LoggerUtility
 from util.common_constants import Constants
 from shortener.shortener import Shortener
 from flask import flash
+import json
 
 APP = Flask(__name__)
 
@@ -12,30 +14,40 @@ LoggerUtility.set_level()
 shortener = Shortener()
 
 
-@APP.route('/shortener', methods=['POST'])
+@APP.route('/shortener', methods=['GET', 'POST'])
 def url_shortener():
     """Rest API for URL Shortener."""
     try:
-        long_url = request.form['url']
-        if not long_url:
-            return jsonify({'error': 'Please provide url'}), 400
+        if request.method == 'GET':
+            return render_template("homepage.html")
+        elif request.method == 'POST':
+            data = json.loads(request.data)
+            long_url = data.get('url')
+            if not long_url:
+                return jsonify({'error': 'Please provide url'}), 400
 
-        response = shortener.url_shortener(long_url)
-        LoggerUtility.log_info(response)
-        if response:
-            # jsonify(short_url=response)
-            return render_template("homepage.html", data=response)
-        return jsonify({'error': 'Internal Server Error'}), 500
+            short_url = shortener.url_shortener(long_url)
+            LoggerUtility.log_info(short_url)
+            if short_url:
+                # jsonify(response), 200
+                response = APP.response_class(
+                    response=json.dumps({'short_url': short_url}),
+                    status=200,
+                    mimetype='application/json'
+                )
+                # return jsonify(json.dumps({'short_url': short_url})), 200
+                return response
+            return jsonify({'error': 'Internal Server Error'}), 500
     except AttributeError as exception:
         LoggerUtility.log_error(exception)
         flash(exception)
         return jsonify(exception), 400
 
 
-@APP.route('/shortener', methods=['GET'])
-def home():
-    """Rest API for URL Shortener."""
-    return render_template("homepage.html")
+# @APP.route('/shortener', methods=['GET'])
+# def home():
+#     """Rest API for URL Shortener."""
+    
 
 
 @APP.route('/<shorturl>', methods=['GET'])
